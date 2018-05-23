b63111db23f8b0a34c9e2d6a041e3880e0cbddfb
Monica Seaman <monica.l.seaman@gmail.com>
2018-05-23 18:45:33 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -9,7 +9,9 @@ class Backer
 
   def back_project(project)
     self.backed_projects << project
-    project.add_backer(self)
+    unless project.backers.include?(self)
+      project.add_backer(self)
+    end
   end
 
 end
